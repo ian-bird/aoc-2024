508869b5632e9ade79ae48525b84c9c1066d74dc
ian-bird <ianukusa@gmail.com>
2024-12-19 11:46:51 -0600

    fix day 16 p1 :)

--- src/day_sixteen.clj
+++ src/day_sixteen.clj
@@ -1,4 +1,4 @@
-(ns day-sixteen 
+(ns day-sixteen
   (:require
    [clojure.set :as set]
    [extension :as e]
@@ -15,91 +15,99 @@
   (if (e/any? nil? args) nil
       (apply + args)))
 
-(def ^:dynamic *maze* nil)
+(defn get-forward
+  [from dir from-cost]
+  {(mapv + from dir) {:dir dir :cost (+ 1 from-cost)}})
 
-(def ^:dynamic *visited* nil)
+(defn get-left
+  [from dir from-cost]
+  (let [left (case dir
+               [0 1] [1 0]
+               [1 0] [0 -1]
+               [0 -1] [-1 0]
+               [-1 0] [0 1])]
+    {(mapv + from left) {:dir left :cost (+ 1001 from-cost)}}))
 
-(defn- do-trace
-  [[current-row current-col] [rel-row rel-col]]
-  (cond (or (>= current-row (count *maze*))
-            (>= current-col (count (first *maze*))))
-        nil
-        :else
-        (let [this-char (-> *maze*
-                            (nth current-row)
-                            (nth current-col))
-              left (case [rel-row rel-col]
-                     [0 1] [1 0]
-                     [1 0] [0 -1]
-                     [0 -1] [-1 0]
-                     [-1 0] [0 1])
-              right (case [rel-row rel-col]
-                      [0 1] [-1 0]
-                      [-1 0] [0 -1]
-                      [0 -1] [1 0]
-                      [1 0] [0 1])]
-          (cond (= this-char "E") 0
-                (contains? *visited* [current-row current-col]) nil
-                (= this-char "#") nil
-                :else
-                (loop [score 1
-                       r current-row
-                       c current-col
-                       sub-visited #{}]
-                  (if (and (-> *maze*
-                               (nth (+ r (first left)))
-                               (nth (+ c (second left)))
-                               (= "#"))
-                           (-> *maze*
-                               (nth (+ r (first right)))
-                               (nth (+ c (second right)))
-                               (= "#"))
-                           (-> *maze*
-                               (nth (+ r rel-row))
-                               (nth (+ c rel-col))
-                               (= ".")))
-                    (recur (inc score) (+ r rel-row) (+ c rel-col) (conj sub-visited [r c]))
-                    (binding [*visited* (set/union *visited*
-                                               #{ [r c]}
-                                               sub-visited)]
-                      (min-ignoring-nils
-                       (list (+without-nils
-                              1000
-                              score
-                              (do-trace [(+ r (first left))
-                                         (+ c (second left))]
-                                        left))
-                             (+without-nils
-                              1000
-                              score
-                              (do-trace [(+ r (first right))
-                                         (+ c (second right))]
-                                        right))
-                             (+without-nils score
-                                            (do-trace
-                                             [(+ r rel-row)
-                                              (+ c rel-col)]
-                                             [rel-row rel-col])))))))))))
+(defn get-right
+  [from dir from-cost]
+  (let [right (case dir
+                [0 1] [-1 0]
+                [-1 0] [0 -1]
+                [0 -1] [1 0]
+                [1 0] [0 1])]
+    {(mapv + from right) {:dir right :cost (+ 1001 from-cost)}}))
 
-  
-  (def do-trace (memoize do-trace))
- 
-(defn trace-path
-  [maze score [current-row current-col] [rel-row rel-col] previously-visited]
-  (binding [*maze* maze 
-            *visited* previously-visited]
-    (do-trace [current-row current-col] [rel-row rel-col])))
+(defn get-possible-steps
+  [square dir cost]
+  (mapv #((eval %) square dir cost) '(get-forward get-left get-right)))
 
- (defn p1
+(defn find-paths
   [maze]
-  (let [s-row (e/index-of-pred #(e/contains? "S" %) maze)
-        s-col (e/index-of-pred (partial = "S") (nth maze s-row))] 
-    (trace-path maze 0 [s-row s-col] [0 1] #{})))
+  (let [get-mins (fn [by coll]
+                   (let [minimum (apply min (map by coll))]
+                     (filter #(= (by %) minimum) coll)))
+        end [(e/index-of-pred #(e/contains? "E" %) maze)
+             (e/index-of-pred
+              (partial = "E")
+              (nth maze (e/index-of-pred #(e/contains? "E" %) maze)))]]
+    (loop [visited-squares
+           {[(e/index-of-pred #(e/contains? "S" %) maze)
+             (e/index-of-pred
+              (partial = "S")
+              (nth maze (e/index-of-pred #(e/contains? "S" %) maze)))]
+            {:dir [0 1] :cost 0}}]
+      (if (contains? visited-squares end)
+        (get visited-squares end)
+        (->> visited-squares
+             ; get the squares
+             keys
+             ; get all the squares that can be walked to from
+             ; each visited square
+             (mapcat (fn [visited-square]
+                       (get-possible-steps visited-square
+                                           (-> visited-squares
+                                               (get visited-square)
+                                               (get :dir))
+                                           (-> visited-squares
+                                               (get visited-square)
+                                               (get :cost)))))
+             ; remove the ones that are out of bounds
+             (filter (fn [considering]
+                       (let [key (first (keys considering))
+                             r (first key)
+                             c (second key)]
+                         (and (>= r 0)
+                              (>= c 0)
+                              (< r (count maze))
+                              (< c (count (first maze)))))))
+             ; remove the ones that are walls
+             (filter (fn [considering]
+                       (->> considering
+                            keys
+                            first
+                            ((fn [[r c]]
+                               (-> maze
+                                   (get r)
+                                   (get c))))
+                            (e/!= "#"))))
+             ; remove the ones we've already visited
+             (filter (fn [considering]
+                       (->> considering
+                            keys
+                            first
+                            (contains? visited-squares)
+                            not)))
+             (get-mins #(get (first (vals %)) :cost))
+             (reduce conj visited-squares)
+             (recur))))))
 
- (let [maze-file "data/day_sixteen/problem"]
+
+
+(let [maze-file "data/day_sixteen/test"]
   (fh/extract-chars maze-file)
   (->> (e/strcat maze-file ".edn")
        slurp
-       read-string 
-       p1
+       read-string
+       find-paths
+       (#(list (get % :cost)))
        time))
