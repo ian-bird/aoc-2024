d32d63f4db53c36814f873bbbbd9cbb979c6b34b
ian-bird <ianukusa@gmail.com>
2024-12-23 18:02:04 -0600

    do d22p2

--- src/day_twenty_two.clj
+++ src/day_twenty_two.clj
@@ -1,5 +1,6 @@
 (ns day-twenty-two 
   (:require 
+   [extension :as e]
    [file-help :as fh]))
 
 (def mix bit-xor)
@@ -14,8 +15,24 @@
         step3 (prune (mix (* 2048 step2) step2))]
     step3))
 
+(defn prices [s] (->> (repeat 2000 nil)
+                        (e/scan (fn [x _] (next-secret x)) s) 
+                           (concat (list s))
+                        (map #(mod % 10))))
 
-(defn get-2000th [s] (reduce (fn [x _] (next-secret x)) s (repeat 2000 nil)))
+(defn sell-sequences
+  [s]
+  (let [p (prices s)]
+    (->> (range 0 5)
+         (map #(repeat % nil))
+         (map #(reduce (fn [x _] (rest x)) p %))
+         (apply e/zip)
+         (map (fn [seq]
+                (list (map #(apply - %) (e/zip-with-rest seq)) (last seq)))) 
+         (e/chunk first)
+         (map last)
+         (mapcat identity)
+         (apply hash-map))))
 
 (let [fn "data/day_twenty_two/problem"]
   (fh/extract-nums-only fn)
@@ -23,5 +40,7 @@
        slurp
        read-string
        (mapcat identity)
-       (pmap get-2000th)
-       (reduce +)))
+       (pmap sell-sequences)
+       (reduce #(merge-with + %1 %2)) 
+       (apply max-key val)
+       time))
